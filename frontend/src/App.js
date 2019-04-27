8b67619588a5410741f7f78eba208f6cf5a7f9a4
sbxio <sbxio@outlook.com>
2019-04-27 12:55:21 -0400

    start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import QrGenerator from "./pages/QrGenerator";
 
 function App() {
-    return <main>hello world!</main>;
+  return (
+    <main>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/qr-generator/">Qr Generator</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/" exact component={() => <div>test</div>} />
+            <Route path="/qr-generator/" component={QrGenerator} />
+          </Switch>
+        </div>
+      </Router>
+    </main>
+  );
 }
 
 export default App;
